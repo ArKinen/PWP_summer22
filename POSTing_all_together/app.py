2314dbb9733e66120685c5e3e0e1bf7c065497cc
Arto Kinos <akinos@student.oulu.fi>
2022-08-11 20:13:41 +0300

    task2_POSTing_all_together

--- POSTing_all_together/app.py
+++ POSTing_all_together/app.py
@@ -6,14 +6,19 @@ from sqlalchemy import event
 from werkzeug.exceptions import HTTPException, NotFound, UnsupportedMediaType, BadRequest, Conflict
 from flask_restful import Resource, Api
 from werkzeug.routing import BaseConverter
+#from flask_caching import Cache
 from jsonschema import validate, ValidationError, draft7_format_checker
+import datetime
 
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+#app.config["CACHE_TYPE"] = "FileSystemCache"
+#app.config["CACHE_DIR"] = "cache"
 db = SQLAlchemy(app)
 api = Api(app)
+#cache = Cache(app)
 
 deployments = db.Table("deployments",
                        db.Column("deployment_id", db.Integer, db.ForeignKey("deployment.id"), primary_key=True),
@@ -56,20 +61,6 @@ class Sensor(db.Model):
     measurements = db.relationship("Measurement", back_populates="sensor")
     deployments = db.relationship("Deployment", secondary=deployments, back_populates="sensors")
 
-
-class Measurement(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    sensor_id = db.Column(db.Integer, db.ForeignKey("sensor.id", ondelete="SET NULL"))
-    value = db.Column(db.Float, nullable=False)
-    time = db.Column(db.DateTime, nullable=False)
-
-    sensor = db.relationship("Sensor", back_populates="measurements")
-
-    def deserialize(self, doc):
-        self.value = doc["name"]
-        doc.update({"time": doc.get("time").fromisoformat()})
-        self.time = doc.get("time")
-
     @staticmethod
     def json_schema():
         schema = {
@@ -84,6 +75,54 @@ class Measurement(db.Model):
         props["model"] = {
             "description": "Name of the sensor's model",
             "type": "string"
+        }
+        props["id"] = {
+            "description": "Sensor id number",
+            "type": "integer"
+        }
+        props["location_id"] = {
+            "description": "Sensor location id number",
+            "type": "integer"
+        }
+        return schema
+
+
+class Measurement(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sensor_id = db.Column(db.Integer, db.ForeignKey("sensor.id", ondelete="SET NULL"))
+    value = db.Column(db.Float, nullable=False)
+    time = db.Column(db.DateTime, nullable=False)
+
+    sensor = db.relationship("Sensor", back_populates="measurements")
+
+    def deserialize(self, doc):
+        print()
+        self.time = datetime.date.fromisoformat(doc["time"])
+        self.value = doc["value"]
+        self.sensor = doc.get("sensor")
+
+    @staticmethod
+    def json_schema():
+        schema = {
+            "type": "object",
+            "required": ["time", "value"]
+        }
+        props = schema["properties"] = {}
+        props["time"] = {
+            "description": "Time the sensor measured the value",
+            "type": "string"
+        }
+        props["value"] = {
+            "description": "Measurement result as a float",
+            "type": "string"
+        }
+        props["id"] = {
+            "description": "Measurement id number",
+            "type": "integer"
+        }
+        props["sensor_id"] = {
+            "description": "Sensor's id number",
+            "type": "integer"
         }
         return schema
 
@@ -171,13 +210,46 @@ class SensorConverter(BaseConverter):
 
 
 class MeasurementCollection(Resource):
-    @staticmethod
-    def get():
-        pass
 
-    @staticmethod
-    def post():
-        pass
+    #def page_key(*args, **kwargs):
+    #    page = request.args.get("page", 0)
+    #    return request.path + f"[page_{page}]"
+    #
+    #PAGE_SIZE = 50
+    #
+    #@cache.cached(timeout=None, make_cache_key=page_key)
+    #def get(self, sensor):
+    #    db_sensor = Sensor.query.filter_by(name=sensor).first()
+    #    if db_sensor is None:
+    #        raise NotFound
+    #    page = request.args.get("page", 0)
+    #    remaining = Measurement.query.filter_by(
+    #        sensor=db_sensor
+    #    ).order_by("time").offset(page * self.PAGE_SIZE)
+    #    body = {
+    #        "sensor": db_sensor.name,
+    #        "measurements": []
+    #    }
+    #    for meas in remaining.limit(self.PAGE_SIZE):
+    #        body["measurements"].append(
+    #            {
+    #                "value": meas.value,
+    #                "time": meas.time.isoformat()
+    #            }
+    #        )
+    #    return Response(json.dumps(body), 200, mimetype=JSON)
+
+    def post(self, sensor):
+        if not request.json:
+            raise UnsupportedMediaType
+        try:
+            validate(request.json, Meas.json_schema())
+        except ValidationError as e:
+            raise BadRequest(description=str(e))
+        sensor.measurements.append(request.json)
+        db.session.add(sensor)
+        db.session.commit()
+        return 201
 
 
 class MeasurementItem(Resource):
