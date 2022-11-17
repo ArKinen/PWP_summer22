bac5ed95d3c72a803cf25887fe8f877c0f72b37f
Miguuli <mikaihamaki@hotmail.com>
2022-11-17 19:43:50 +0200

    Test code update. 5/5 models input complete.

--- REST_api/app.py
+++ REST_api/app.py
@@ -48,6 +48,7 @@ class Recipe(db.Model):
 
 class Compartment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), nullable=False, unique=True)
     location = db.relationship("Location", back_populates="compartments")
     location_id = db.Column(db.Integer, db.ForeignKey("location.id"), unique=True)
     ingredients = db.relationship("Ingredient", back_populates="compartments")
@@ -58,7 +59,7 @@ class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(16), nullable=False)
     amount = db.Column(db.Integer, nullable=True)
-    compartment_id = db.Column(db.Integer, db.ForeignKey("compartment.id"), unique=True)
+    compartment_id = db.Column(db.Integer, db.ForeignKey("compartment.id"), unique=False)
     compartments = db.relationship("Compartment", back_populates="ingredients")
 
 
@@ -73,35 +74,92 @@ class Location(db.Model):
 def init_db_command():
     db.create_all()
 
+
+@click.command("roll")
+@with_appcontext
+def rollback_db_command():
+    db.session.rollback()
+
+
 @click.command("testgen")
 @with_appcontext
 def generate_test_data():
     db.session.rollback()
-    recipe_ingredients = ["Chicken leg", "Mashed potatoes", "Ketchup", "Rice", "Minced meat", "Kebab"]
-    recipes = ["Mac and Cheese", "Chicken and Potatoes", "Rice and Kebab"]
-    for recipe in range(0, len(recipes)):
+    all_recipes = ["Mac and Cheese", "Chicken and Potatoes", "Rice and Kebab"]
+    compartments = ["Veggies", "Meat", "Mixed", "Add-ons"]
+    ingredients_veggies = ["Cucumber", "Salad", "Tomato", "Paprika"]
+    ingredients_mixed = ["Mashed Potatoes", "Rice"]
+    ingredients_add_ons = ["Ketchup"]
+    ingredients_meat = ["Chicken leg", "Minced meat", "Kebab"]
+    locations = ["Fridge", "Freezer"]
+    recipe_categories = ["Starters", "Main", "Dessert"]
+    recipe_ingredients = ingredients_veggies + ingredients_meat + ingredients_mixed + ingredients_add_ons
+
+    input_recipe_category(recipe_categories)
+    input_location(locations)
+    input_recipe(recipe_ingredients, all_recipes)
+    input_compartment(compartments)
+    input_ingredient(recipe_ingredients)
+
+
+def input_recipe_category(_recipe_categories):
+    for count, category in enumerate(_recipe_categories):
+        category_model = RecipeCategory(
+            course_type= str(category)
+        )
+        db.session.add(category_model)
+        db.session.commit()
+
+
+def input_location(_location):
+    for count, location_name in enumerate(_location):
+        location_model = Location(
+            name=str(location_name)
+        )
+        db.session.add(location_model)
+        db.session.commit()
+
+
+def input_recipe(recipe_ingredients, _all_recipes):
+    for recipe in range(0, len(_all_recipes)):
         used_recipe_ingredients = ""
         for i in range(0, 2):
-            random_number = random.randint(0, (len(recipe_ingredients)-1))
-            print(f"Join ingredient- {recipe_ingredients[random_number]} to {recipes[recipe]}")
+            random_number = random.randint(0, (len(recipe_ingredients) - 1))
+            # print(f"Join ingredient- {recipe_ingredients[random_number]} to {_all_recipes[recipe]}")
             if used_recipe_ingredients == "":
+                # print(f"recipe_ingredients{recipe_ingredients[random_number]} not in {used_recipe_ingredients}")
                 used_recipe_ingredients = recipe_ingredients[random_number]
-            used_recipe_ingredients = used_recipe_ingredients + "," + recipe_ingredients[random_number]
+            elif recipe_ingredients[random_number] not in used_recipe_ingredients:
+                used_recipe_ingredients = used_recipe_ingredients + "," + recipe_ingredients[random_number]
+            else:
+                break
 
         recipe_model = Recipe(
-            title=recipes[recipe],
+            title=_all_recipes[recipe],
             ingredient=used_recipe_ingredients
         )
         db.session.add(recipe_model)
         db.session.commit()
 
-    for count, ingredient in enumerate(recipe_ingredients):
 
+def input_compartment(_compartments):
+    for count, compartment in enumerate(_compartments):
+        compartment_model = Compartment(
+            name=str(compartment)
+        )
+        db.session.add(compartment_model)
+        db.session.commit()
+
+
+def input_ingredient(_recipe_ingredients):
+    compartments_load_from_db = Compartment.query.all()
+    for count, ingredient in enumerate(_recipe_ingredients):
         ingredient_model = Ingredient(
             name=str(ingredient),
-            amount=random.randint(0,9)
+            amount=random.randint(0, 9),
+            compartments=compartments_load_from_db[random.randint(0, 3)]
         )
-
+        # print(f"{ingredient_model.name} in {ingredient_model.compartments}")
         db.session.add(ingredient_model)
         db.session.commit()
 
@@ -126,3 +184,4 @@ def handle_exception(e):
 
 app.cli.add_command(init_db_command)
 app.cli.add_command(generate_test_data)
+app.cli.add_command(rollback_db_command)
