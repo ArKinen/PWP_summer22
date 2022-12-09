6a7920c3468f48c9a8030a4842b4461c866d4a30
Arto Kinos <akinos@student.oulu.fi>
2022-12-09 19:20:54 +0200

    web site base upgrade

--- REST_api/static/scripts/admin.js
+++ REST_api/static/scripts/admin.js
@@ -22,18 +22,94 @@ function renderMsg(msg) {
     $("div.notification").html("<p class='msg'>" + msg + "</p>");
 }
 
+function recipeRow(item) {
+    let link = "<a href='" +
+                item["@controls"].self.href +
+                "' onClick='followLink(event, this, renderRecipe)'>show</a>";
+
+    return "<tr><td>" + item.title +
+            "</td><td>" + item.course +
+            "</td><td>" + item.ingredient +
+            "</td><td>" + link + "</td></tr>";
+}
+
+function renderRecipe(body) {
+    $("div.navigation")
+        .html(
+        "<a href='" +
+        body["@controls"].collection.href +
+        "' onClick='followLink(event, this, renderRecipes)'>Collection</a>"
+        )
+
+    $("div.tablecontrols").empty() //ARKI
+
+    $(".resulttable thead").empty();
+    $(".resulttable tbody").empty();
+    renderSensorForm(body["@controls"].edit);
+    $("input[name='title']").val(body.title);
+    $("input[name='course']").val(body.course);
+   // $("form input[type='submit']").before(
+     //   "<label>Location</label>" +
+       // "<input type='text' name='location' value='" +
+      //  body.location + "' readonly>"
+   // );
+}
+function renderIngredients(body) {
+
+    $("div.form").empty();
+    $(".resulttable thead").html(
+"<tr><th>Name</th><th>Amount</th></tr>"
+    );
+
+    $("div.navigation")
+        .html("<a href='" + body["@controls"].self.href +
+        "' onClick='followLink(event, this, renderIngredients)'> Ingredients</a>" + "<br>"
+        )
+
+    let val = $("div.tablecontrols").empty()
+
+
+    let tbody = $(".resulttable tbody");
+    tbody.empty();
+    getResource(body["@controls"].self.href)
+    body.items.forEach(function (item) {
+        tbody.append("<tr><td>" + item.time +
+            "</td><td>" + item.value +
+            "</td></tr>");
+    });
+}
+
 function renderRecipes(body) {
     $("div.navigation").empty();
     $("div.tablecontrols").empty();
-    //$(".resulttable thead").html(
-    //    "<tr><th>Name</th><th>Model</th><th>Location</th><th>Actions</th></tr>"
-    //);
-    //let tbody = $(".resulttable tbody");
-    //tbody.empty();
-
-    //renderSensorForm(body["@controls"]["recipe:add-sensor"]);
+    $(".resulttable thead").html(
+        "<tr><th>Title</th><th>Course</th><th>Ingredient</th></tr>"
+    );
+    let tbody = $(".resulttable tbody");
+    tbody.empty();
+    body.items.forEach(function (item) {
+        tbody.append(recipeRow(item));
+    });
+    renderRecipeForm(body["@controls"]["recipe:add-sensor"]);
 }
 
+function renderRecipeForm(ctrl) {
+    let form = $("<form>");
+    let title = ctrl.schema.properties.title;
+    let course = ctrl.schema.properties.course;
+    form.attr("action", ctrl.href);
+    form.attr("method", ctrl.method);
+    form.submit(submitSensor);
+    form.append("<label>" + title.description + "</label>");
+    form.append("<input type='text' name='name'>");
+    form.append("<label>" + course.description + "</label>");
+    form.append("<input type='text' name='model'>");
+    ctrl.schema.required.forEach(function (property) {
+        $("input[name='" + property + "']").attr("required", true);
+    });
+    form.append("<input type='submit' name='submit' value='Submit'>");
+    $("div.form").html(form);
+}
 
 function sendData(href, method, item, postProcessor) {
     $.ajax({
